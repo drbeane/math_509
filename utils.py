7c95ab48cab5702094a73c1c93a201002aabb491
drbeane <37283017+drbeane@users.noreply.github.com>
2023-11-08 11:16:15 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,13 @@
-def gradient_optimization(fn, vars, init_vals, alpha, max_iter,
+def gradient_optimization(sym_fn, vars, init_vals, alpha, max_iter,
                           threshold, mode='min', verbosity=0):
 
-    import numpy as np
-    import sympy as sym
-
-    gradient = sym.derive_by_array(fn, vars)
+    gradient = sym.derive_by_array(sym_fn, vars)
 
     params = np.array(init_vals)
     param_list = [params]
 
     sub_dict = {var:val for var, val in zip(vars, params)}
-    f_val = fn.subs(sub_dict)
+    f_val = sym_fn.subs(sub_dict)
 
     found = False
 
@@ -53,13 +50,11 @@ def gradient_optimization(fn, vars, init_vals, alpha, max_iter,
         return None, param_list
 
 
-def single_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[6,3]):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    f_values = [fn.subs({a:b for a,b in zip(vars, est) }) for est in param_list]
+def single_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[6,3], yscale=None):
+    f_values = [fn(x[0]) for x in param_list]
 
     x_grid = np.linspace(xlim[0], xlim[1], 100)
-    y_grid = [fn.subs({x:x_}) for x_ in x_grid]
+    y_grid = fn(x_grid)a
 
     plt.figure(figsize=fs)
     plt.plot(x_grid, y_grid)
@@ -70,14 +65,12 @@ def single_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[6,3]):
     plt.scatter(param_list[-1], f_values[-1], c='darkorange', edgecolor='k', s=40, zorder=2)
     plt.xlim(xlim)
     plt.ylim(ylim)
-    #plt.yscale('symlog')
+    if yscale is not None:
+        plt.yscale(yscale)
 
     plt.show()
 
-
-def two_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[12,6]):
-    import numpy as np
-    import matplotlib.pyplot as plt
+def two_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[12,6], zoom=True):
 
     # x and y coords for points along gradient path
     x_path = [float(params[0]) for params in param_list]
@@ -96,7 +89,7 @@ def two_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[12,6]):
     dx, dy = b-a, d-c
     X = np.vstack([np.linspace(a-dx, b+dx, N)]*N)
     Y = np.vstack([np.linspace(c-dy, d+dy, N)]*N).T
-    h = F(X,Y)
+    h = fn(X,Y)
 
     # Define Levels
     lmax = h.max(); lmin = h.min()
@@ -106,7 +99,8 @@ def two_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[12,6]):
     plt.figure(figsize=fs)
 
     # Large plot
-    plt.subplot(1, 2, 1)
+    if zoom:
+        plt.subplot(1, 2, 1)
     plt.contourf(X, Y, h, levels=lvls, cmap='RdBu')
     plt.contour(X, Y, h, levels=lvls, linestyles='solid', linewidths=1, colors='#777777', zorder=3)
     plt.plot(x_path, y_path, c='k', zorder=5)
@@ -117,17 +111,17 @@ def two_var_grad_plot(fn, vars, param_list, xlim, ylim, fs=[12,6]):
     plt.ylim(ylim)
     plt.grid()
 
-    # Window
-    plt.subplot(1, 2, 2)
-    plt.contourf(X, Y, h, levels=lvls, cmap='RdBu')
-    plt.contour(X, Y, h, levels=lvls, linestyles='solid', linewidths=1, colors='#777777', zorder=3)
-    plt.plot(x_path, y_path, c='k', zorder=5)
-    plt.scatter(x_path, y_path, c='k', zorder=5, s=10)
-    plt.scatter(x_path, y_path, c='darkorange', zorder=4, s=30)
-    plt.axis('On')
-    plt.xlim(win_x)
-    plt.ylim(win_y)
-    plt.grid()
+    if zoom:
+        # Window
+        plt.subplot(1, 2, 2)
+        plt.contourf(X, Y, h, levels=lvls, cmap='RdBu')
+        plt.contour(X, Y, h, levels=lvls, linestyles='solid', linewidths=1, colors='#777777', zorder=3)
+        plt.plot(x_path, y_path, c='k', zorder=5)
+        plt.scatter(x_path, y_path, c='k', zorder=5, s=10)
+        plt.scatter(x_path, y_path, c='darkorange', zorder=4, s=30)
+        plt.axis('On')
+        plt.xlim(win_x)
+        plt.ylim(win_y)
+        plt.grid()
 
     plt.show()
-
